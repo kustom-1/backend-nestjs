3b66405fce48580c77032f92f4639b3f03932fe5
RaulQode <raulqode@gmail.com>
2025-10-28 22:38:34 -0500

    including all new entities modules to app nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,18 @@ import { AuthModule } from './auth/auth.module';
 import { StorageModule } from './storage/storage.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { AuditModule } from './audit/audit.module';
+import { ClothsModule } from './cloths/cloths.module';
+import { CategoriesModule } from './categories/categories.module';
+import { DesignsModule } from './designs/designs.module';
+import { ImagesModule } from './images/images.module';
+import { CartsModule } from './carts/carts.module';
+import { CustomImagesModule } from './custom-images/custom-images.module';
+import { CartDesignModule } from './cart-design/cart-design.module';
+import { StocksModule } from './stocks/stocks.module';
+import { AddressesModule } from './addresses/addresses.module';
+import { OrdersModule } from './orders/orders.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { DesignHistoryModule } from './design-history/design-history.module';
 import { LoggerMiddleware } from './common/LoggerMiddleware';
 
 @Module({
@@ -13,7 +25,7 @@ import { LoggerMiddleware } from './common/LoggerMiddleware';
     ConfigModule.forRoot({ isGlobal: true }),
     // Conexión a MongoDB para auditoría
     MongooseModule.forRoot(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/sgd-vivienda-audit',
+      process.env.MONGODB_URI || 'mongodb://localhost:27017//myapp',
       {
         // Opciones de conexión
         retryWrites: true,
@@ -25,6 +37,18 @@ import { LoggerMiddleware } from './common/LoggerMiddleware';
     AuthModule,
     PermissionsModule,
     AuditModule,
+    ClothsModule,
+    CategoriesModule,
+    DesignsModule,
+    ImagesModule,
+    CartsModule,
+    CustomImagesModule,
+    CartDesignModule,
+    StocksModule,
+    AddressesModule,
+    OrdersModule,
+    TransactionsModule,
+    DesignHistoryModule,
   ],
 })
 export class AppModule implements NestModule {
