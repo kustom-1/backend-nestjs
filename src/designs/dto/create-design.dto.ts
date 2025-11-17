f964926d9ddb2c7eef690af029f6522bd5ffaa40
RaulQode <raulqode@gmail.com>
2025-11-17 17:04:26 -0500

    feat: added payload column to desing entity

--- src/designs/dto/create-design.dto.ts
+++ src/designs/dto/create-design.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsInt, IsBoolean } from 'class-validator';
+import { IsOptional, IsInt, IsBoolean, IsObject } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDesignDto {
@@ -21,4 +21,22 @@ export class CreateDesignDto {
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
+
+  @ApiProperty({
+    required: false,
+    example: {
+      baseModel: '/shirt.glb',
+      baseColor: '#ffffff',
+      decal: {
+        imageUrl: '[inline-data-uri]',
+        position: [-0.19964031679170438, 3.230041632845925, -23.165392805743632],
+        rotation: [-89.99999167528105, -7.655592358121921e-8, -7.65559124581224e-8],
+        scale: 0.35,
+        aspectRatio: 0.5625,
+      },
+    },
+  })
+  @IsOptional()
+  @IsObject()
+  payload?: any;
 }
