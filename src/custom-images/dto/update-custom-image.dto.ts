f173d8584582c5017e5e71d2af0f600e1b8b6b4f
Raul Quigua <raulqode@gmail.com>
2025-11-23 23:34:00 -0500

    deleted all modules and REST impl

--- src/custom-images/dto/update-custom-image.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCustomImageDto } from './create-custom-image.dto';
-
-export class UpdateCustomImageDto extends PartialType(CreateCustomImageDto) {}
--- src/design-history/dto/update-design-history.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDesignHistoryDto } from './create-design-history.dto';
-
-export class UpdateDesignHistoryDto extends PartialType(CreateDesignHistoryDto) {}
